d946c527844d03987f19f33335d79590249606b1
Oscar Bautista <oscar04262000@gmail.com>
2024-05-24 03:28:09 -0600

    Probar nueva funcionalidad

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -49,91 +49,127 @@ class _HomeScreenState extends State<HomeScreen> {
         padding: EdgeInsets.only(top: 10.0, left: 20.0, right: 20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const Text('¡Bienvendido ....!', style: TextStyle( fontSize: 25, fontWeight: FontWeight.bold, color: Color.fromRGBO(2, 63, 120, 1)),),
-            const SizedBox(height: 30,),
-            CardContainerHome(
-              child: SingleChildScrollView (
-                scrollDirection: Axis.horizontal,
-                child: DataTable(
-                  headingTextStyle: const TextStyle(color: Colors.white),
-                  dataTextStyle: const TextStyle(color: Colors.white),
-                  columns: const <DataColumn>[
-                    DataColumn(label: Expanded(child: Text('Clientes'))),
-                    DataColumn(label: Expanded(child: Text('Ciudad'),)),
-                  ],
-                  rows:  <DataRow> [
-                    DataRow(cells: <DataCell>[
-                      DataCell(Row(
-                        children: [
-                          CircleAvatar(backgroundColor: Colors.blue[500], child: const Text('JM'),),
-                          const SizedBox(width: 8),
-                          const Text('José Martinez Robledo'),
-                        ],
-                      )),
-                      const DataCell(Text('Puebla, México'))
-                    ]),
-                    const DataRow(cells: <DataCell>[
-                      DataCell( Row(
-                        children: [
-                          CircleAvatar(backgroundImage: NetworkImage('https://www.kindpng.com/picc/m/24-245173_dama-profesor-perfil-la-cabeza-mujeres-mujer-user.png'), ),
-                          SizedBox(width: 8,),
-                          Text('Angela Gómez Pelaes')
-                        ],
-                      )),
-                      DataCell(Text('CDMX, México'))
-                    ]),
-                    const DataRow(cells: <DataCell>[
-                      DataCell( Row(
-                        children: [
-                          CircleAvatar(backgroundImage: NetworkImage('https://gorental.es/wp-content/uploads/2022/01/face_icons-circle-59.png'),),
-                          SizedBox(width: 8,),
-                          Text('Miguel Pérez Gutierrez')
-                        ],
-                      )),
-                      DataCell(Text('CDMX, México'))
-                    ]),
-                  ],
-                )
+            Expanded(
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  int crossAxisCount = constraints.maxWidth > 600 ? 2 : 1;
+                  return GridView.count(
+                    crossAxisCount: crossAxisCount,
+                    crossAxisSpacing: 16.0,
+                    mainAxisSpacing: 16.0,
+                    children: [
+                      _TableClient(),
+                      _MenuOption()
+                    ],
+                  );
+                },
               ) 
             ),
-            const SizedBox(height: 40,),
-            CardContainerHome(
-              child: Column(
-                children: [
-                  TextButton.icon(
-                    onPressed: () {
-                      final route = MaterialPageRoute(builder: (context) => FormNewCreditScreen());
-                      Navigator.push(context, route);
-                    },
-                    icon: const Icon(Icons.playlist_add_outlined, color: Colors.white,),
-                    label: const Text('Nueva solicitud', style: TextStyle(color: Colors.white),),
-                    style: TextButton.styleFrom(
-                      backgroundColor: const Color.fromRGBO(237, 128, 12, 1),
-                      fixedSize: const Size(180, 50)
-                    ),
-                  ),
-                  const SizedBox(height: 10,),
-                  TextButton.icon(
-                    onPressed: () {
-                      final route = MaterialPageRoute(builder: (context) => const SearchClientScreen());
-                      Navigator.push(context, route);
-                    },
-                    icon: const Icon(Icons.search, color: Colors.white,),
-                    label: const Text('Buscar cliente', style: TextStyle(color: Colors.white), ),
-                    style: TextButton.styleFrom(
-                      backgroundColor: const Color.fromRGBO(237, 128, 12, 1),
-                      fixedSize: const Size(180, 50),
-                    ),
-                  ),
-                  const SizedBox(height: 10,),
-
-                ],
-              ),
-            )
+            
           ],
         ),
       )
+    );
+  }
+}
+
+class _MenuOption extends StatelessWidget {
+  const _MenuOption({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CardContainerHome(
+      child: Column(
+        children: [
+          TextButton.icon(
+            onPressed: () {
+              final route = MaterialPageRoute(builder: (context) => FormNewCreditScreen());
+              Navigator.push(context, route);
+            },
+            icon: const Icon(Icons.playlist_add_outlined, color: Colors.white,),
+            label: const Text('Nueva solicitud', style: TextStyle(color: Colors.white),),
+            style: TextButton.styleFrom(
+              backgroundColor: const Color.fromRGBO(237, 128, 12, 1),
+              fixedSize: const Size(180, 50)
+            ),
+          ),
+          const SizedBox(height: 10,),
+          TextButton.icon(
+            onPressed: () {
+              final route = MaterialPageRoute(builder: (context) => const SearchClientScreen());
+              Navigator.push(context, route);
+            },
+            icon: const Icon(Icons.search, color: Colors.white,),
+            label: const Text('Buscar cliente', style: TextStyle(color: Colors.white), ),
+            style: TextButton.styleFrom(
+              backgroundColor: const Color.fromRGBO(237, 128, 12, 1),
+              fixedSize: const Size(180, 50),
+            ),
+          ),
+          const SizedBox(height: 10,),
+    
+        ],
+      ),
+    );
+  }
+}
+
+class _TableClient extends StatelessWidget {
+  const _TableClient({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CardContainerHome(
+      child: SingleChildScrollView (
+        scrollDirection: Axis.horizontal,
+        child: DataTable(
+          headingTextStyle: const TextStyle(color: Colors.white),
+          dataTextStyle: const TextStyle(color: Colors.white),
+          columns: const <DataColumn>[
+            DataColumn(label: Expanded(child: Text('Clientes'))),
+            DataColumn(label: Expanded(child: Text('Ciudad'),)),
+          ],
+          rows:  <DataRow> [
+            DataRow(cells: <DataCell>[
+              DataCell(Row(
+                children: [
+                  CircleAvatar(backgroundColor: Colors.blue[500], child: const Text('JM'),),
+                  const SizedBox(width: 8),
+                  const Text('José Martinez Robledo'),
+                ],
+              )),
+              const DataCell(Text('Puebla, México'))
+            ]),
+            const DataRow(cells: <DataCell>[
+              DataCell( Row(
+                children: [
+                  CircleAvatar(backgroundImage: NetworkImage('https://www.kindpng.com/picc/m/24-245173_dama-profesor-perfil-la-cabeza-mujeres-mujer-user.png'), ),
+                  SizedBox(width: 8,),
+                  Text('Angela Gómez Pelaes')
+                ],
+              )),
+              DataCell(Text('CDMX, México'))
+            ]),
+            const DataRow(cells: <DataCell>[
+              DataCell( Row(
+                children: [
+                  CircleAvatar(backgroundImage: NetworkImage('https://gorental.es/wp-content/uploads/2022/01/face_icons-circle-59.png'),),
+                  SizedBox(width: 8,),
+                  Text('Miguel Pérez Gutierrez')
+                ],
+              )),
+              DataCell(Text('CDMX, México'))
+            ]),
+          ],
+        )
+      ) 
     );
   }
 }
